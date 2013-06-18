31e302d799d5584c95604f68f797abc34844074f
Nayelie Benitez <nayeliebenitez@yahoo.com>
2013-06-18 14:07:23 -0700

    Changing tor- Nayelie/Angel

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -1,5 +1,7 @@
 package org.teachingkidsprogramming.recipes;
 
+import java.awt.Color;
+
 import org.teachingextensions.logo.Tortoise;
 
 public class SimpleSquare
@@ -10,8 +12,8 @@ public class SimpleSquare
     //  Make the tortoise move as fast as possible --#6
     //  Do the following 4 times --#5.1
     //      Change the color of the line the tortoise draws to "blue" --#4
+    Tortoise.setPenColor(Color.blue);
     Tortoise.move(50);
-    //      Turn the tortoise to the right (90 degrees) --#3
     Tortoise.turn(90);
     //  Repeat --#5.2
   }
